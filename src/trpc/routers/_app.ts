f4691f64a09daa560c99336482f76d27faea2649
NamKhanhIT2 <khanha1k59@gmail.com>
2026-05-22 15:51:16 +0700

    26: suggestions

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -3,6 +3,7 @@ import { videosRouter } from '@/modules/videos/server/procedures';
 import { commentsRouter } from '@/modules/comments/server/procedures';
 import { videoViewsRouter } from '@/modules/video-views/server/procedures';
 import { categoriesRouter } from '@/modules/categories/server/procedures';
+import { suggestionsRouter } from '@/modules/suggestions/server/procedures';
 import { subscriptionsRouter } from '@/modules/subscriptions/server/procedures';
 import { videoReactionsRouter } from '@/modules/video-reactions/server/procedures';
 import { commentReactionsRouter } from '@/modules/comment-reactions/server/procedures';
@@ -16,5 +17,6 @@ export const appRouter = createTRPCRouter({
   videoReactions: videoReactionsRouter,
   comments: commentsRouter,
   commentReactions: commentReactionsRouter,
+  suggestions: suggestionsRouter,
 });
 export type AppRouter = typeof appRouter;
