bbd4ed243b64add48a97434b44ec2c49519be504
NamKhanhIT2 <khanha1k59@gmail.com>
2026-05-22 11:28:44 +0700

    23: comments infinite loading

--- src/app/(home)/videos/[videoId]/page.tsx
+++ src/app/(home)/videos/[videoId]/page.tsx
@@ -1,5 +1,5 @@
 import { HydrateClient, trpc } from "@/trpc/server";
-
+import { DEFAULT_LIMIT } from "@/constants";
 import { VideoView } from "@/modules/videos/ui/views/video-view";
 
 interface PageProps {
@@ -11,7 +11,7 @@ interface PageProps {
 const Page = async ({ params }: PageProps) => {
   const { videoId } = await params;
   void trpc.videos.getOne.prefetch({ id: videoId });
-  void trpc.comments.getMany.prefetch({ videoId: videoId });
+  void trpc.comments.getMany.prefetch({ videoId: videoId, limit: DEFAULT_LIMIT });
   return ( 
     <HydrateClient>
       <VideoView videoId={videoId} />
