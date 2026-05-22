edab38b676001c5b7afb3c6e60e16d781a438dd1
NamKhanhIT2 <khanha1k59@gmail.com>
2026-05-22 10:27:23 +0700

    21: subscription

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,14 +1,18 @@
 import { studioRouter } from '@/modules/studio/server/procedures';
 import { videosRouter } from '@/modules/videos/server/procedures';
+import { commentsRouter } from '@/modules/comments/server/procedures';
 import { videoViewsRouter } from '@/modules/video-views/server/procedures';
 import { categoriesRouter } from '@/modules/categories/server/procedures';
+import { subscriptionsRouter } from '@/modules/subscriptions/server/procedures';
 import { videoReactionsRouter } from '@/modules/video-reactions/server/procedures';
 import { createTRPCRouter } from '../init';
 export const appRouter = createTRPCRouter({
   studio: studioRouter,
   videos: videosRouter,
+  subscriptions: subscriptionsRouter,
   categories: categoriesRouter,
   videoViews: videoViewsRouter,
   videoReactions: videoReactionsRouter,
+  comments: commentsRouter,
 });
 export type AppRouter = typeof appRouter;
