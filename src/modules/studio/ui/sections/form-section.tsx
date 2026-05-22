6427911e9fd774e03916f5039aef151c70713109
NamKhanhIT2 <khanha1k59@gmail.com>
2026-05-22 17:59:56 +0700

    27: search page

--- src/modules/studio/ui/sections/form-section.tsx
+++ src/modules/studio/ui/sections/form-section.tsx
@@ -40,7 +40,7 @@ import {
 } from "@/components/ui/select";
 import { THUMBNAIL_FALLBACK } from "@/modules/videos/constants";
 import { VideoPlayer } from "@/modules/videos/ui/components/video-player";
-
+import { APP_URL } from "@/constants";
 import { ThumbnailUploadModal } from "../components/thumbnail-upload-modal";
 interface FormSectionProps {
   videoId: string;
@@ -142,6 +142,16 @@ const FormSectionSuspense = ({ videoId }: FormSectionProps) => {
       toast.error("Something went wrong");
     },
   });
+  const revalidate = trpc.videos.revalidate.useMutation({
+    onSuccess: () => {
+      utils.studio.getMany.invalidate();
+      utils.studio.getOne.invalidate({ id: videoId });
+      toast.success("Video revalidated");
+    },
+    onError: () => {
+      toast.error("Something went wrong");
+    },
+  });
   const generateDescription = trpc.videos.generateDescription.useMutation({
     onSuccess: () => {
       toast.success("Background job started", { description: "This may take some time" });
@@ -180,7 +190,7 @@ const FormSectionSuspense = ({ videoId }: FormSectionProps) => {
   };
 
 
-  const fullUrl = `${process.env.VERCEL_URL || "http://localhost:3000"}/videos/${videoId}`;
+  const fullUrl = `${APP_URL || "http://localhost:3000"}/videos/${videoId}`;
   const [isCopied, setIsCopied] = useState(false);
 
   const onCopy = async () => {
@@ -222,6 +232,10 @@ const FormSectionSuspense = ({ videoId }: FormSectionProps) => {
                   </Button>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent align="end">
+                  <DropdownMenuItem onClick={() => revalidate.mutate({ id: videoId })}>
+                    <RotateCcwIcon className="size-4 mr-2" />
+                    Revalidate
+                  </DropdownMenuItem>
                   <DropdownMenuItem onClick={() => remove.mutate({ id: videoId })}>
                     <TrashIcon className="size-4 mr-2" />
                     Delete
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,5 +1,6 @@
 import { studioRouter } from '@/modules/studio/server/procedures';
 import { videosRouter } from '@/modules/videos/server/procedures';
+import { searchRouter } from '@/modules/search/server/procedures';
 import { commentsRouter } from '@/modules/comments/server/procedures';
 import { videoViewsRouter } from '@/modules/video-views/server/procedures';
 import { categoriesRouter } from '@/modules/categories/server/procedures';
@@ -11,6 +12,7 @@ import { createTRPCRouter } from '../init';
 export const appRouter = createTRPCRouter({
   studio: studioRouter,
   videos: videosRouter,
+  search: searchRouter,
   subscriptions: subscriptionsRouter,
   categories: categoriesRouter,
   videoViews: videoViewsRouter,
