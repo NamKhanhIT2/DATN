bcca2af97b06734dd8e688f0db1da37af53113d0
NamKhanhIT2 <khanha1k59@gmail.com>
2026-05-25 14:54:37 +0700

    33: individual playlist

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -146,13 +146,13 @@ const VideosSectionSuspense = () => {
                     {format(new Date(video.createdAt), "d MMM yyyy")}
                   </TableCell>
                   <TableCell className="text-right">
-                    views
+                    {video.viewCount}
                   </TableCell>
                   <TableCell className="text-right text-sm">
-                    comments
+                    {video.commentCount}
                   </TableCell>
                   <TableCell className="text-right text-sm pr-6">
-                    likes
+                    {video.likeCount}
                   </TableCell>
                 </TableRow>
               </Link>
